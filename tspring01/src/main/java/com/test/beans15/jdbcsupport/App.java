99804fb2b919d4f4ad13c9789a3ad122c9772e94
lasterWang <17591429@qq.com>
2016-10-11 15:39:47 +0800

    d

--- tspring01/src/main/java/com/test/beans15/jdbcsupport/App.java
+++ tspring01/src/main/java/com/test/beans15/jdbcsupport/App.java
@@ -1,16 +1,33 @@
 package com.test.beans15.jdbcsupport;
 
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
 
 public class App {
-
-	public static void main(String[] args) {
-		ApplicationContext ctx=new ClassPathXmlApplicationContext("beans-jdbcTeamplate.xml");
-		ICalc calc=(ICalc) ctx.getBean(ICalc.class);
-		System.out.println(calc.add(1, 2));
-		System.out.println(calc.sub(1, 2));
-		System.out.println(calc.mul(1, 2));
-		System.out.println(calc.div(1, 2));
+	
+	private ApplicationContext ctx=null;
+	private JdbcTemplate jdbcTemplate=null;
+	
+	{
+		ctx=new ClassPathXmlApplicationContext("beans-jdbcTeamplate.xml");
+		jdbcTemplate=(JdbcTemplate) ctx.getBean("jdbcTemplate");
+	}
+	@Test
+	public void testDataSource() throws SQLException
+	{
+		DataSource ds=ctx.getBean(DataSource.class);
+		System.out.println(ds.getConnection());
+	}
+	
+	@Test
+	public void update()
+	{
+		
 	}
 }
