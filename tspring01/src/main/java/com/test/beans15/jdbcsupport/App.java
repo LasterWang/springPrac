6ba0bf7302b5591870cabbf933058a4017e53fe5
lasterWang <17591429@qq.com>
2016-10-12 07:19:50 +0800

    d

--- tspring01/src/main/java/com/test/beans15/jdbcsupport/App.java
+++ tspring01/src/main/java/com/test/beans15/jdbcsupport/App.java
@@ -1,13 +1,17 @@
 package com.test.beans15.jdbcsupport;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.sql.DataSource;
 
 import org.junit.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 
 public class App {
 	
@@ -18,6 +22,7 @@ public class App {
 		ctx=new ClassPathXmlApplicationContext("beans-jdbcTeamplate.xml");
 		jdbcTemplate=(JdbcTemplate) ctx.getBean("jdbcTemplate");
 	}
+	//测试连接是否成功
 	@Test
 	public void testDataSource() throws SQLException
 	{
@@ -25,9 +30,53 @@ public class App {
 		System.out.println(ds.getConnection());
 	}
 	
+	//更新
 	@Test
 	public void update()
 	{
-		int res=jdbcTemplate.update("update user set name='bbb' where id=1");
+//		int res=jdbcTemplate.update("update user set name='bbb' where id=1");
+		String sql="update user set name=? where id=?";
+		jdbcTemplate.update(sql,"JACK",1);
+	}
+	//批量更新
+	@Test
+	public void batchUpdate()
+	{
+		String sql="insert into user(id,name,age,dept_id) values(?,?,?,?)";
+		List<Object[]> batchArgs=new ArrayList<Object[]>();
+		
+		batchArgs.add(new Object[]{6,"张三","18",2});
+		batchArgs.add(new Object[]{7,"王五","19",2});
+		batchArgs.add(new Object[]{8,"马六","20",2});
+		
+		jdbcTemplate.batchUpdate(sql, batchArgs);
+	}
+	//查询一条记录，得到一个对象
+	//1.RowMapper:指定映射结果集的行，常用实现类：BeanPropertyRowMapper
+	//2.使用sql 中列的别名完成列明和类属性的映射。
+	//3.不支持级联属性，jdbcTemplate是个jdbc工具而非ORM工具。
+	@Test
+	public void testQueryForObject(){
+		String sql="select id,name,age,dept_id as \"dept.id\" from user where id=?";
+		
+		RowMapper<User> rMapper=new BeanPropertyRowMapper<User>(User.class);
+		User user=jdbcTemplate.queryForObject(sql, rMapper,1);
+		System.out.println(user);
+	}
+	
+	//查询实体类的集合
+	@Test
+	public void testQueryForList(){
+		String sql="select id,name,age from user where id>?";
+		RowMapper<User> rMapper=new BeanPropertyRowMapper<User>(User.class);
+		List<User> userList=jdbcTemplate.query(sql, rMapper,1);
+		System.out.println(userList);
+	}
+	//获取单个列的值，或做统计查询
+	@Test
+	public void queryForObject2(){
+		String sql="select count(id) from user";
+		int count=jdbcTemplate.queryForObject(sql, Integer.class);
+		System.out.println(count);
 	}
 }
---
+++ tspring01/src/main/java/com/test/beans15/jdbcsupport/User.java
@@ -0,0 +1,37 @@
+package com.test.beans15.jdbcsupport;
+
+public class User {
+	private Integer id;
+	private String name;
+	private Integer age;
+	private Dept dept;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	public Dept getDept() {
+		return dept;
+	}
+	public void setDept(Dept dept) {
+		this.dept = dept;
+	}
+	@Override
+	public String toString() {
+		return "User [id=" + id + ", name=" + name + ", age=" + age + ", dept=" + dept + "]";
+	}
+	
+}
